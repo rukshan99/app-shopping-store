b5c8b70ea18491fdfbc0d4759cb05151d8b9b88a
Rukshan Jayasekara <rukshanjayasekara@outlook.com>
2021-05-02 15:31:25 +0530

    implemented signin signup components with related shared components

---
+++ src/user/SignIn.js
@@ -0,0 +1,139 @@
+import React, { useState, useContext } from 'react';
+
+import Input from '../shared/FormElements/Input';
+import Button from '../shared/FormElements/Button';
+import Card from '../shared/UIElements/Card';
+import ErrorModal from '../shared/UIElements/ErrorModal';
+import LoadingSpinner from '../shared/UIElements/LoadingSpinner';
+
+import { useForm } from '../../shared/hooks/form-hooks';
+import { VALIDATOR_EMAIL, VALIDATOR_MINLENGTH, VALIDATOR_REQUIRE } from '../../shared/util/validators';
+import { useHttpClient } from '../../shared/hooks/http-hook';
+import { AuthContext } from '../../shared/context/auth-context';
+import './SignIn.css';
+
+const SignIn = () => {
+    const auth = useContext(AuthContext);
+    const [isSignInMode, setIsSignInMode] = useState(true);
+    const { isLoading, error, sendRequest, clearError } = useHttpClient();
+
+    const [formState, inputHandler, setFormData] = useForm(
+        {
+            email: {
+                value: '',
+                isValid: false
+            },
+            password: {
+                value: '',
+                isValid: false
+            }
+        },
+        false
+    );
+
+    const switchModeHandler = () => {
+        if(!isSignInMode) {
+            setFormData(
+                {
+                    ...formState.inputs,
+                    name: undefined
+                },
+                formState.inputs.email.isValid && formState.inputs.password.isValid);
+        }
+        else {
+            setFormData({
+                ...formState.inputs,
+                name: {
+                    value: '',
+                    isValid: false
+                }
+            }, false);
+        }
+        setIsSignInMode(prevMode => !prevMode);
+    };
+
+    const signInSubmitHandler = async event => {
+        event.preventDefault();
+
+        if(isSignInMode) {
+            try{
+                const responseData = await sendRequest(
+                    'http://localhost:5000/api/users/signin', 
+                    'POST', 
+                    JSON.stringify({
+                        email: formState.inputs.email.value,
+                        password: formState.inputs.password.value
+                    }),
+                    {
+                        'Content-Type': 'application/json'
+                    }
+                );
+
+                auth.SignIn(responseData.user.id); 
+            } catch (err) {}
+        } else {
+            try {
+                const responseData = await sendRequest(
+                    'http://localhost:5000/api/users/signup', 
+                    'POST',
+                    JSON.stringify({
+                        name: formState.inputs.name.value,
+                        email: formState.inputs.email.value,
+                        password: formState.inputs.password.value
+                    }),
+                    {
+                        'Content-Type': 'application/json',
+                    }
+                );
+                auth.SignIn(responseData.user.id); 
+            } catch(err) {}
+        }
+    };
+    
+    return (
+        <React.Fragment>
+        <ErrorModal error={error} onClear={clearError}/>    
+        <Card className="authentication">
+            {isLoading && <LoadingSpinner asOverlay/>}
+            <h2>Please Sign in</h2>
+            <hr />
+            <form onSubmit={signInSubmitHandler}>
+                {!isSignInMode && (
+                    <Input 
+                        id="name"
+                        element="input" 
+                        type="text" 
+                        lable="Full name" 
+                        validators={[VALIDATOR_REQUIRE()]} 
+                        errorText="Please enter your name." 
+                        onInput={inputHandler}
+                    />
+                )}
+                <Input 
+                    id="email"
+                    element="input" 
+                    type="email" 
+                    lable="E-mail" 
+                    validators={[VALIDATOR_EMAIL()]} 
+                    errorText="Please enter a valid E-mail." 
+                    onInput={inputHandler}
+                />
+                <Input 
+                    id="password"
+                    element="input" 
+                    type="password" 
+                    lable="Password" 
+                    validators={[VALIDATOR_MINLENGTH(6)]} 
+                    errorText="Password should be at least 6 characters long." 
+                    onInput={inputHandler}
+                />
+                <Button type="submit" disabled={!formState.isValid}>{isSignInMode ? 'Sign in' : 'Sign up'}</Button>
+            </form>
+            <Button inverse onClick={switchModeHandler}>{isSignInMode ? 'Sign up' : 'Sign in'}</Button>
+        </Card>
+        </React.Fragment>
+    );
+};
+
+
+export default SignIn;
