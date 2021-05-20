1dd3ca77e75e47e0d8baa1e98c7fa832e8e0ea5b
SJayasinghe <68707752+SJayasinghe@users.noreply.github.com>
2021-05-20 23:27:29 +0530

    delivery service

--- src/App.js
+++ src/App.js
@@ -2,7 +2,9 @@ import React from 'react';
 import {BrowserRouter as Router} from 'react-router-dom'
 import Header from './shared/Navigation/Header'
 import Section from './shared/Navigation/Section'
+import DeliveryDetails from './DeliveryDetails/DeliveryDetails'
 import {DataProvider} from './Products/components/Context'
+
 
 class App extends React.Component{
   render(){
@@ -12,7 +14,14 @@ class App extends React.Component{
           <Router>
             <Header />
             <Section />
+            
           </Router>
+          <Router>
+            <DeliveryDetails/>
+          </Router>
+
+
+        
         </div>
       </DataProvider>
     );
