c71a99e9d912e8a2c30a60e9b1d829131303d75b
SJayasinghe <68707752+SJayasinghe@users.noreply.github.com>
2021-05-21 00:06:06 +0530

    Fix routing conflict

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import {BrowserRouter as Router} from 'react-router-dom'
+import {Route} from 'react-router-dom';
 import Header from './shared/Navigation/Header'
 import Section from './shared/Navigation/Section'
 import DeliveryDetails from './DeliveryDetails/DeliveryDetails'
 import {DataProvider} from './Products/components/Context'
+import PaymentGateway from './Payment/payment-gateway';
 
 
 class App extends React.Component{
